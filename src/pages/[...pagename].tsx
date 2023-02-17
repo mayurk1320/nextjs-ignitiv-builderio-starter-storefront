ffdfbe22245c56238a0063186a1131d518f4bc78
kibo-kevinwatts <85258296+kibo-kevinwatts@users.noreply.github.com>
2023-02-17 15:10:56 -0600

    remove category slider reference

--- src/pages/[...pagename].tsx
+++ src/pages/[...pagename].tsx
@@ -2,7 +2,6 @@ import { BuilderComponent, builder } from '@builder.io/react'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import getConfig from 'next/config'
 
-import nextI18NextConfig from '../../next-i18next.config'
 import getCategoryTree from '@/lib/api/operations/get-category-tree'
 import type { CategoryTreeResponse } from '@/lib/types'
 
@@ -39,7 +38,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     props: {
       page: page || null,
       categoriesTree,
-      ...(await serverSideTranslations(locale as string, ['common'], nextI18NextConfig)),
+      ...(await serverSideTranslations(locale as string, ['common'])),
     },
   }
 }
