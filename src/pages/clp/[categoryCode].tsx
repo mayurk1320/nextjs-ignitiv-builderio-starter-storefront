b0fe03ef913f677806a6c59548c4cc5c6e25cac8
Souvik-13254 <souvik.p@ignitiv.com>
2024-01-04 15:51:50 +0530

    clp page in dev progress

---
+++ src/pages/clp/[categoryCode].tsx
@@ -0,0 +1,60 @@
+import { BuilderComponent, builder } from '@builder.io/react'
+import getConfig from 'next/config'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+import getCategoryTree from '@/lib/api/operations/get-category-tree'
+import type { CategoryTreeResponse } from '@/lib/types'
+
+import type { GetServerSidePropsContext } from 'next'
+
+const { publicRuntimeConfig } = getConfig()
+const apiKey = publicRuntimeConfig?.builderIO?.apiKey
+
+builder.init(apiKey)
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { locale } = context
+
+  const pathnameArr = context.params?.pagename
+
+  let pagename
+  if (Array.isArray(pathnameArr) && pathnameArr?.length > 1) {
+    pagename = pathnameArr.join('/')
+  } else {
+    pagename = pathnameArr
+  }
+  console.log('pagename', pagename, pathnameArr)
+
+  const categoriesTree: CategoryTreeResponse = await getCategoryTree()
+
+  const page = await builder
+    .get(publicRuntimeConfig?.builderIO?.modelKeys?.defaultPage, {
+      userAttributes: {
+        urlPath: `/${pagename}`,
+      },
+    })
+    .toPromise()
+
+  return {
+    props: {
+      page: page || null,
+      categoriesTree,
+      ...(await serverSideTranslations(locale as string, ['common'])),
+    },
+  }
+}
+
+const Page = (props: any) => {
+  const { page } = props
+  return (
+    <div>
+      <p>i am souvik</p>
+      <BuilderComponent
+        model={publicRuntimeConfig?.builderIO?.modelKeys?.defaultPage}
+        content={page}
+      />
+    </div>
+  )
+}
+
+export default Page
